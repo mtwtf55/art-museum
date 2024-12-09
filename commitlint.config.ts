768ab21c97fbfb4b66d36db67f5785547069f709
mtwtf55 <d.nkfrv.55@gmail.com>
2024-12-09 11:43:31 +0300

    chore: make additional setup for project. Add eslint, prettier, husky, commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,10 @@
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  formatter: "@commitlint/format",
+  rules: {},
+};
+
+export default Configuration;
