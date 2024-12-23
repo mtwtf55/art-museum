4e1383347f9db9b463c554b44a95840440895d95
mtwtf55 <d.nkfrv.55@gmail.com>
2024-12-24 00:50:45 +0300

    refactor: made style theme for the app. Theme placed in _theme.scss

--- src/components/Container/Container.tsx
+++
@@ -1,9 +0,0 @@
-import "./Container.scss";
-
-import React, { PropsWithChildren } from "react";
-
-function Container({ children }: PropsWithChildren) {
-  return <div className={"container"}>{children}</div>;
-}
-
-export default Container;
